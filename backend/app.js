c4afba615e30af35b5d8b6df27c736b42b890e60
Rajat <85779110+rajatDeveloper@users.noreply.github.com>
2023-07-02 13:12:54 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,30 @@
 const cookieParser = require("cookie-parser");
-const express = require("express")
+const express = require("express");
 const errorMiddleware = require("./middleware/error");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-var cors = require('cors');
+var cors = require("cors");
 const dotenv = require("dotenv");
 const path = require("path");
-
 
 // require("dotenv").config({ path: "backend/config/config.env" });
 const app = express();
 
 const corsOrigin = {
-    origin: 'http://localhost:3000', //or whatever port your frontend is using
-    credentials: true,
-    optionSuccessStatus: 200
-}
+  origin: "http://localhost:3000", //or whatever port your frontend is using
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsOrigin));
 if (process.env.NODE_ENV !== "PRODUCTION") {
-    require("dotenv").config({ path: "backend/config/config.env" });
+  require("dotenv").config({ path: "backend/config/config.env" });
 }
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
-// route imports  
+// route imports
 // var bodyParser = require('body-parser');
 
 const product = require("./routes/productRoute");
@@ -34,21 +33,26 @@ const order = require("./routes/orderRoute");
 const payment = require("./routes/paymentRoute");
 
 app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+// app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
 
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-});
-
-// middleware for error  
+// middleware for error
 app.use(errorMiddleware);
 
-module.exports = app 
+module.exports = app;
